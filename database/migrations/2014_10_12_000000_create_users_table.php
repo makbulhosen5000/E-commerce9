0f28f6369de01a7ed392e8655d26494cbacec676
mhakash5000 <mhakash5000@gmail.com>
2022-04-01 21:08:59 +0600

    admin dashboard

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,14 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('role');
             $table->string('name');
+            $table->string('phone');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->default();
+            $table->string('image')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,16 @@
 <?php
 
+use App\Http\Controllers\Backend\AboutController;
+use App\Http\Controllers\Backend\ContactController;
+use App\Http\Controllers\Backend\LogoController;
+use App\Http\Controllers\Backend\MissionController;
+use App\Http\Controllers\Backend\NewseventController;
+use App\Http\Controllers\Backend\ProfileController;
+use App\Http\Controllers\Backend\ServiceController;
+use App\Http\Controllers\Backend\SliderController;
+use App\Http\Controllers\Backend\UserController;
+use App\Http\Controllers\Backend\VisionController;
+use App\Http\Controllers\Frontend\FrontendController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +27,131 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//all header(frontend page)routes start from here
+Route::get('/',[FrontendController::class,'index']);
+Route::get('aboutUs',[FrontendController::class,'AboutUs'])->name('aboutUs');
+Route::get('contactUs',[FrontendController::class,'contactUs'])->name('contactUs');
+Route::get('mission',[FrontendController::class,'MiSsion'])->name('mission');
+Route::get('vision',[FrontendController::class,'ViSion'])->name('vision');
+Route::get('newsevent.details/{id}',[FrontendController::class,'NewsEventDetails'])->name('newsevent.details');
+Route::post('Contact.Store',[FrontendController::class,'Store'])->name('Contact.Store');
+
+// group middleware start from here
+Route::group(['middleware' => ['test']], function () {
+//all header(backend page)routes start from here
+Route::prefix('users')->group(function () {
+Route::get('/all',[UserController::class,'index'])->name('users.all');
+Route::get('/create',[UserController::class,'create'])->name('users.create');
+Route::post('/store',[UserController::class,'store'])->name('users.store');
+Route::get('/edit/{id}',[UserController::class,'edit'])->name('users.edit');
+Route::post('/update/{id}',[UserController::class,'update'])->name('users.update');
+Route::get('/destroy/{id}',[UserController::class,'destroy'])->name('users.destroy');
+
+});
+
+// Profile Routes start from here
+Route::prefix('profile')->group(function () {
+Route::get('/user',[UserController::class,'index'])->name('profile.user');
+Route::post('/store',[UserController::class,'store'])->name('profile.store');
+Route::get('/edit',[UserController::class,'edit'])->name('profile.edit');
+Route::post('/update',[UserController::class,'update'])->name('profile.update');
+Route::get('/change-password',[ProfileController::class,'ChangePassword'])->name('change.password');
+Route::post('/update-password',[ProfileController::class,'UpdatePassword'])->name('update.password');
+
+});
+
+
+
+//Logo routes start from here
+Route::prefix('logos')->group(function () {
+Route::get('/view',[LogoController::class,'index'])->name('logos.view');
+Route::get('/create',[LogoController::class,'create'])->name('logos.create');
+Route::post('/store',[LogoController::class,'store'])->name('logos.store');
+Route::get('/edit/{id}',[LogoController::class,'edit'])->name('logos.edit');
+Route::post('/update/{id}',[LogoController::class,'update'])->name('logos.update');
+Route::get('/destroy/{id}',[LogoController::class,'destroy'])->name('logos.destroy');
+});
+
+//Sliders routes start from here
+Route::prefix('sliders')->group(function () {
+Route::get('/view',[SliderController::class,'index'])->name('sliders.view');
+Route::get('/create',[SliderController::class,'create'])->name('sliders.create');
+Route::post('/store',[SliderController::class,'store'])->name('sliders.store');
+Route::get('/edit/{id}',[SliderController::class,'edit'])->name('sliders.edit');
+Route::post('/update/{id}',[SliderController::class,'update'])->name('sliders.update');
+Route::get('/destroy/{id}',[SliderController::class,'destroy'])->name('sliders.destroy');
+});
+
+
+//missions routes start from here
+Route::prefix('missions')->group(function () {
+Route::get('/view',[MissionController::class,'index'])->name('missions.view');
+Route::get('/create',[MissionController::class,'create'])->name('missions.create');
+Route::post('/store',[MissionController::class,'store'])->name('missions.store');
+Route::get('/edit/{id}',[MissionController::class,'edit'])->name('missions.edit');
+Route::post('/update/{id}',[MissionController::class,'update'])->name('missions.update');
+Route::get('/destroy/{id}',[MissionController::class,'destroy'])->name('missions.destroy');
+});
+
+//Vision routes start from here
+Route::prefix('visions')->group(function () {
+Route::get('/view',[VisionController::class,'index'])->name('visions.view');
+Route::get('/create',[VisionController::class,'create'])->name('visions.create');
+Route::post('/store',[VisionController::class,'store'])->name('visions.store');
+Route::get('/edit/{id}',[VisionController::class,'edit'])->name('visions.edit');
+Route::post('/update/{id}',[VisionController::class,'update'])->name('visions.update');
+Route::get('/destroy/{id}',[VisionController::class,'destroy'])->name('visions.destroy');
+});
+
+//Newsevent routes start from here
+Route::prefix('newsevents')->group(function () {
+Route::get('/view',[NewseventController::class,'index'])->name('newsevents.view');
+Route::get('/create',[NewseventController::class,'create'])->name('newsevents.create');
+Route::post('/store',[NewseventController::class,'store'])->name('newsevents.store');
+Route::get('/edit/{id}',[NewseventController::class,'edit'])->name('newsevents.edit');
+Route::post('/update/{id}',[NewseventController::class,'update'])->name('newsevents.update');
+Route::get('/destroy/{id}',[NewseventController::class,'destroy'])->name('newsevents.destroy');
+});
+
+//services routes start from here
+Route::prefix('services')->group(function () {
+Route::get('/view',[ServiceController::class,'index'])->name('services.view');
+Route::get('/create',[ServiceController::class,'create'])->name('services.create');
+Route::post('/store',[ServiceController::class,'store'])->name('services.store');
+Route::get('/edit/{id}',[ServiceController::class,'edit'])->name('services.edit');
+Route::post('/update/{id}',[ServiceController::class,'update'])->name('services.update');
+Route::get('/destroy/{id}',[ServiceController::class,'destroy'])->name('services.destroy');
+});
+
+//services routes start from here
+Route::prefix('contacts')->group(function () {
+Route::get('/view',[ContactController::class,'index'])->name('contacts.view');
+Route::get('/create',[ContactController::class,'create'])->name('contacts.create');
+Route::post('/store',[ContactController::class,'store'])->name('contacts.store');
+Route::get('/edit/{id}',[ContactController::class,'edit'])->name('contacts.edit');
+Route::post('/update/{id}',[ContactController::class,'update'])->name('contacts.update');
+Route::get('/destroy/{id}',[ContactController::class,'destroy'])->name('contacts.destroy');
+});
+
+//services routes start from here
+Route::prefix('about')->group(function () {
+Route::get('/view',[AboutController::class,'destroy'])->name('about.view');
+Route::get('/create',[AboutController::class,'create'])->name('about.create');
+Route::post('/store',[AboutController::class,'store'])->name('about.store');
+Route::get('/edit/{id}',[AboutController::class,'edit'])->name('about.edit');
+Route::post('/update/{id}',[AboutController::class,'update'])->name('about.update');
+Route::get('/destroy/{id}',[AboutController::class,'destroy'])->name('about.destroy');
+});
+
+//User Email route
+Route::prefix('email')->group(function () {
+Route::get('user-email.view',[FrontendController::class,'UserEmailView'])->name('user-email.view');
+Route::get('/user-email.destroy/{id}',[FrontendController::class,'destroy'])->name('user-email.destroy');
+});
+});
+// group middleware End here
